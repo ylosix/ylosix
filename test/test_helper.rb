e5dd91a34b876644f1fc4dd010f443b28184ba62
Guillermo Guerrero <wolf.fox1985@gmail.com>
2015-04-29 00:10:26 +0200

    Added fixtures.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,6 +14,10 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+class ActionController::TestCase
+  include Devise::TestHelpers
+end
+
 class TestApp < ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical
   # order.
