574804874fee580f8362cdf691747b6df21b9fd3
Guillermo Guerrero <wolf.fox1985@gmail.com>
2015-05-19 14:28:05 +0200

    Fixed config puma for heroku.

--- config/puma.rb
+++ config/puma.rb
@@ -8,8 +8,13 @@ rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RAILS_ENV'] || 'development'
 
-pidfile ENV['RAILS_PIDS'] || 'tmp/pids/puma.pid'
-stdout_redirect ENV['RAILS_LOGS'] || 'log/puma.log', ENV['RAILS_LOGS'] || 'log/puma.log'
+if ENV['RAILS_PIDS'].nil? && ENV['RAILS_PIDS'] != ''
+  pidfile ENV['RAILS_PIDS']
+end
+
+if ENV['RAILS_LOGS'].nil? && ENV['RAILS_LOGS'] != ''
+  stdout_redirect ENV['RAILS_LOGS'], ENV['RAILS_LOGS']
+end
 
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
