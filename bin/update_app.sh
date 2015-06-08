14e8feb5c5f672eb52c8bbd540e6105740d66c7b
Guillermo Guerrero <wolf.fox1985@gmail.com>
2015-06-08 11:25:03 +0200

    Added script update app.

---
+++ bin/update_app.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+git fetch origin;
+git pull origin;
+
+bundle install --without development test profile
+
+rake RAILS_ENV=production update_app
+
+chown -R www-data:www-data /var/www/public
