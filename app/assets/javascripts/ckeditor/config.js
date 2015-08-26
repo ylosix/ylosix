51d644618f0719d83e84fa00c83ae29e6a7c2eb9
Guillermo Guerrero Ibarra <wolf.fox1985@gmail.com>
2015-08-26 11:06:55 +0200

    Fixed compiled asset.

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,137 +1,139 @@
-CKEDITOR.editorConfig = function (config) {
-    config.allowedContent = true;
-    config.extraAllowedContent = 'p(*)[*]{*};div(*)[*]{*};li(*)[*]{*};ul(*)[*]{*}';
-    config.enterMode = CKEDITOR.ENTER_BR;
+if (typeof CKEDITOR != 'undefined') {
+    CKEDITOR.editorConfig = function (config) {
+        config.allowedContent = true;
+        config.extraAllowedContent = 'p(*)[*]{*};div(*)[*]{*};li(*)[*]{*};ul(*)[*]{*}';
+        config.enterMode = CKEDITOR.ENTER_BR;
 
-    // Define changes to default configuration here. For example:
-    config.language = 'en';
-    // config.uiColor = '#AADC6E';
+        // Define changes to default configuration here. For example:
+        config.language = 'en';
+        // config.uiColor = '#AADC6E';
 
-    /* Filebrowser routes */
-    // The location of an external file browser, that should be launched when "Browse Server" button is pressed.
-    config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
+        /* Filebrowser routes */
+        // The location of an external file browser, that should be launched when "Browse Server" button is pressed.
+        config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
 
-    // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Flash dialog.
-    config.filebrowserFlashBrowseUrl = "/ckeditor/attachment_files";
+        // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Flash dialog.
+        config.filebrowserFlashBrowseUrl = "/ckeditor/attachment_files";
 
-    // The location of a script that handles file uploads in the Flash dialog.
-    config.filebrowserFlashUploadUrl = "/ckeditor/attachment_files";
+        // The location of a script that handles file uploads in the Flash dialog.
+        config.filebrowserFlashUploadUrl = "/ckeditor/attachment_files";
 
-    // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Link tab of Image dialog.
-    config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
+        // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Link tab of Image dialog.
+        config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
 
-    // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Image dialog.
-    config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
+        // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Image dialog.
+        config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
 
-    // The location of a script that handles file uploads in the Image dialog.
-    config.filebrowserImageUploadUrl = "/ckeditor/pictures";
+        // The location of a script that handles file uploads in the Image dialog.
+        config.filebrowserImageUploadUrl = "/ckeditor/pictures";
 
-    // The location of a script that handles file uploads.
-    config.filebrowserUploadUrl = "/ckeditor/attachment_files";
+        // The location of a script that handles file uploads.
+        config.filebrowserUploadUrl = "/ckeditor/attachment_files";
 
-    // Rails CSRF token
-    config.filebrowserParams = function () {
-        var csrf_token, csrf_param, meta,
-            metas = document.getElementsByTagName('meta'),
-            params = new Object();
+        // Rails CSRF token
+        config.filebrowserParams = function () {
+            var csrf_token, csrf_param, meta,
+                metas = document.getElementsByTagName('meta'),
+                params = new Object();
 
-        for (var i = 0; i < metas.length; i++) {
-            meta = metas[i];
+            for (var i = 0; i < metas.length; i++) {
+                meta = metas[i];
 
-            switch (meta.name) {
-                case "csrf-token":
-                    csrf_token = meta.content;
-                    break;
-                case "csrf-param":
-                    csrf_param = meta.content;
-                    break;
-                default:
-                    continue;
+                switch (meta.name) {
+                    case "csrf-token":
+                        csrf_token = meta.content;
+                        break;
+                    case "csrf-param":
+                        csrf_param = meta.content;
+                        break;
+                    default:
+                        continue;
+                }
             }
-        }
 
-        if (csrf_param !== undefined && csrf_token !== undefined) {
-            params[csrf_param] = csrf_token;
-        }
+            if (csrf_param !== undefined && csrf_token !== undefined) {
+                params[csrf_param] = csrf_token;
+            }
 
-        return params;
+            return params;
+        };
+
+        config.addQueryString = function (url, params) {
+            var queryString = [];
+
+            if (!params) {
+                return url;
+            } else {
+                for (var i in params)
+                    queryString.push(i + "=" + encodeURIComponent(params[i]));
+            }
+
+            return url + ( ( url.indexOf("?") != -1 ) ? "&" : "?" ) + queryString.join("&");
+        };
+
+        // Integrate Rails CSRF token into file upload dialogs (link, image, attachment and flash)
+        CKEDITOR.on('dialogDefinition', function (ev) {
+            // Take the dialog name and its definition from the event data.
+            var dialogName = ev.data.name;
+            var dialogDefinition = ev.data.definition;
+            var content, upload;
+
+            if (CKEDITOR.tools.indexOf(['link', 'image', 'attachment', 'flash'], dialogName) > -1) {
+                content = (dialogDefinition.getContents('Upload') || dialogDefinition.getContents('upload'));
+                upload = (content == null ? null : content.get('upload'));
+
+                if (upload && upload.filebrowser && upload.filebrowser['params'] === undefined) {
+                    upload.filebrowser['params'] = config.filebrowserParams();
+                    upload.action = config.addQueryString(upload.action, upload.filebrowser['params']);
+                }
+            }
+        });
+
+        // Toolbar groups configuration.
+        config.toolbar = [
+            {name: 'document', groups: ['mode', 'document', 'doctools'], items: ['Source']},
+            {
+                name: 'clipboard',
+                groups: ['clipboard', 'undo'],
+                items: ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo']
+            },
+            // { name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] },
+            // { name: 'forms', items: [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField' ] },
+            {name: 'links', items: ['Link', 'Unlink', 'Anchor']},
+            {name: 'insert', items: ['Image', 'Flash', 'Table', 'HorizontalRule', 'SpecialChar']},
+            {
+                name: 'paragraph',
+                groups: ['list', 'indent', 'blocks', 'align', 'bidi'],
+                items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock']
+            },
+            '/',
+            {name: 'styles', items: ['Styles', 'Format', 'Font', 'FontSize']},
+            {name: 'colors', items: ['TextColor', 'BGColor']},
+            {
+                name: 'basicstyles',
+                groups: ['basicstyles', 'cleanup'],
+                items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat']
+            }
+        ];
+
+        config.toolbar_mini = [
+            {
+                name: 'paragraph',
+                groups: ['list', 'indent', 'blocks', 'align', 'bidi'],
+                items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock']
+            },
+            {name: 'styles', items: ['Font', 'FontSize']},
+            {name: 'colors', items: ['TextColor', 'BGColor']},
+            {
+                name: 'basicstyles',
+                groups: ['basicstyles', 'cleanup'],
+                items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat']
+            },
+            {name: 'insert', items: ['Image', 'Table', 'HorizontalRule', 'SpecialChar']}
+        ];
     };
 
-    config.addQueryString = function (url, params) {
-        var queryString = [];
-
-        if (!params) {
-            return url;
-        } else {
-            for (var i in params)
-                queryString.push(i + "=" + encodeURIComponent(params[i]));
-        }
-
-        return url + ( ( url.indexOf("?") != -1 ) ? "&" : "?" ) + queryString.join("&");
-    };
-
-    // Integrate Rails CSRF token into file upload dialogs (link, image, attachment and flash)
-    CKEDITOR.on('dialogDefinition', function (ev) {
-        // Take the dialog name and its definition from the event data.
-        var dialogName = ev.data.name;
-        var dialogDefinition = ev.data.definition;
-        var content, upload;
-
-        if (CKEDITOR.tools.indexOf(['link', 'image', 'attachment', 'flash'], dialogName) > -1) {
-            content = (dialogDefinition.getContents('Upload') || dialogDefinition.getContents('upload'));
-            upload = (content == null ? null : content.get('upload'));
-
-            if (upload && upload.filebrowser && upload.filebrowser['params'] === undefined) {
-                upload.filebrowser['params'] = config.filebrowserParams();
-                upload.action = config.addQueryString(upload.action, upload.filebrowser['params']);
-            }
-        }
-    });
-
-    // Toolbar groups configuration.
-    config.toolbar = [
-        {name: 'document', groups: ['mode', 'document', 'doctools'], items: ['Source']},
-        {
-            name: 'clipboard',
-            groups: ['clipboard', 'undo'],
-            items: ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo']
-        },
-        // { name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] },
-        // { name: 'forms', items: [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField' ] },
-        {name: 'links', items: ['Link', 'Unlink', 'Anchor']},
-        {name: 'insert', items: ['Image', 'Flash', 'Table', 'HorizontalRule', 'SpecialChar']},
-        {
-            name: 'paragraph',
-            groups: ['list', 'indent', 'blocks', 'align', 'bidi'],
-            items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock']
-        },
-        '/',
-        {name: 'styles', items: ['Styles', 'Format', 'Font', 'FontSize']},
-        {name: 'colors', items: ['TextColor', 'BGColor']},
-        {
-            name: 'basicstyles',
-            groups: ['basicstyles', 'cleanup'],
-            items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat']
-        }
-    ];
-
-    config.toolbar_mini = [
-        {
-            name: 'paragraph',
-            groups: ['list', 'indent', 'blocks', 'align', 'bidi'],
-            items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock']
-        },
-        {name: 'styles', items: ['Font', 'FontSize']},
-        {name: 'colors', items: ['TextColor', 'BGColor']},
-        {
-            name: 'basicstyles',
-            groups: ['basicstyles', 'cleanup'],
-            items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat']
-        },
-        {name: 'insert', items: ['Image', 'Table', 'HorizontalRule', 'SpecialChar']}
-    ];
-};
-
-// not remove [i, b] tags
-CKEDITOR.dtd.$removeEmpty.i = 0;
-CKEDITOR.dtd.$removeEmpty.b = 0;
+    // not remove [i, b] tags
+    CKEDITOR.dtd.$removeEmpty.i = 0;
+    CKEDITOR.dtd.$removeEmpty.b = 0;
+}
