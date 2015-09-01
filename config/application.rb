9540b880d4ddd96084f92cd1b8f35d52130cd899
Guillermo Guerrero Ibarra <wolf.fox1985@gmail.com>
2015-09-01 10:08:41 +0200

    Added config inline job adapter.

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Ecommerce
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.active_job.queue_adapter = :inline
     config.i18n.default_locale = :en
     config.i18n.available_locales = [:es, :en]
 
