02397cbf9fb709870533b6bebc1447ebaec85073
Guillermo Guerrero Ibarra <wolf.fox1985@gmail.com>
2015-11-17 13:35:22 +0100

    Fixed exception.

--- app/models/utils.rb
+++ app/models/utils.rb
@@ -63,7 +63,7 @@ class Utils
   end
 
   def self.elem_respond_to_liquid(elem)
-    hash = elem.dup unless elem.blank? && elem.class == Symbol
+    hash = elem.dup if !elem.blank? && elem.class != Symbol
     hash = elem.to_liquid if elem.respond_to?(:to_liquid)
 
     if hash.class == Array
