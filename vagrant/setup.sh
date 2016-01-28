e7041aa76cadeadcc8a54b997eb7890f122f098e
Guillermo Guerrero Ibarra <wolf.fox1985@gmail.com>
2016-01-28 12:34:34 +0100

    Fix if already prepared.

--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -9,6 +9,9 @@ sudo gem install mailcatcher
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 
+# Removed if already prepared
+rm -rf ./tmp/container_prepared
+
 # Run and link the containers
 docker run -d -p 5432:5432 -v /vagrant:/vagrant -e "POSTGRES_PASSWORD=postgres" --name postgres postgres:9.4.1
 docker run -d -p 3000:3000 -v /vagrant:/var/www --privileged --link postgres:db \
