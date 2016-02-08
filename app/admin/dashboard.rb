7ce5cadf0de705ab27c5b957efb3ec73f51630a9
Guillermo Guerrero Ibarra <wolf.fox1985@gmail.com>
2016-02-08 10:30:54 +0100

    Restyles. Updated gems.

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -6,169 +6,177 @@ ActiveAdmin.register_page 'Dashboard' do
     config_db = current_config.database_configuration[Rails.env]
     # updates = `cd #{Rails.root}; git fetch; git rev-list HEAD...origin/develop --count`
 
-    commerce = Commerce.find_by(default: true)
-    unless commerce.nil?
-      panel 'Default site' do
-        columns do
-          column do
-            span t('activerecord.models.commerce.one')
-          end
+    columns do
+      commerce = Commerce.find_by(default: true)
+      unless commerce.nil?
+        column do
+          panel 'Default site' do
+            columns do
+              column do
+                span t('activerecord.models.commerce.one')
+              end
 
-          column do
-            span auto_link commerce
+              column do
+                span auto_link commerce
+              end
+            end
+
+            columns do
+              column do
+                span t('activerecord.attributes.commerce.enable_commerce_options')
+              end
+
+              column do
+                commerce.enable_commerce_options ? 'Yes' : 'No'
+              end
+            end
+
+            columns do
+              column do
+                span 'Http'
+              end
+
+              column do
+                span commerce.http
+              end
+            end
+
+            columns do
+              column do
+                span t('activerecord.attributes.commerce.name')
+              end
+
+              column do
+                span commerce.name
+              end
+            end
+
+            if commerce && commerce.enable_commerce_options
+              columns do
+                column do
+                  span 'Billing address'
+                end
+
+                column do
+                  span commerce.billing_address
+                end
+              end
+            end
+
+            columns do
+              column do
+                span 'Logo'
+              end
+
+              column do
+                (img src: commerce.logo.url(:original)) if commerce.logo?
+              end
+            end
+
+            columns do
+              column do
+                span t('activerecord.attributes.commerce.template')
+              end
+
+              column do
+                span auto_link commerce.template
+              end
+            end
           end
         end
+      end
 
-        columns do
-          column do
-            span t('activerecord.attributes.commerce.enable_commerce_options')
-          end
-
-          column do
-            commerce.enable_commerce_options ? 'Yes' : 'No'
-          end
-        end
-
-        columns do
-          column do
-            span 'Http'
-          end
-
-          column do
-            span commerce.http
-          end
-        end
-
-        columns do
-          column do
-            span t('activerecord.attributes.commerce.name')
-          end
-
-          column do
-            span commerce.name
-          end
-        end
-
-        if commerce && commerce.enable_commerce_options
+      column do
+        panel 'Frontend variables' do
           columns do
             column do
-              span 'Billing address'
+              span 'Debug variables'
             end
 
             column do
-              span commerce.billing_address
+              span current_admin_user.debug_variables ? 'Yes' : 'No'
+            end
+          end
+
+          columns do
+            column do
+              span 'Debug template'
+            end
+
+            column do
+              span auto_link current_admin_user.debug_template
             end
           end
         end
+      end
 
-        columns do
-          column do
-            span 'Logo'
+      column do
+        panel 'Application variables' do
+          columns do
+            column do
+              span 'Version'
+            end
+
+            column do
+              span Ecommerce::Application::VERSION.dup
+            end
           end
 
-          column do
-            (img src: commerce.logo.url(:original)) if commerce.logo?
+          columns do
+            column do
+              span 'Environment'
+            end
+
+            column do
+              span Rails.env
+            end
           end
-        end
 
-        columns do
-          column do
-            span t('activerecord.attributes.commerce.template')
+          columns do
+            column do
+              span 'Root folder'
+            end
+
+            column do
+              span Rails.root
+            end
           end
 
-          column do
-            span auto_link commerce.template
+          columns do
+            column do
+              span 'Database'
+            end
+
+            column do
+              span config_db['database']
+            end
           end
+
+          columns do
+            column do
+              span 'Database adapter'
+            end
+
+            column do
+              span config_db['adapter']
+            end
+          end
+
+          # columns do
+          #   column do
+          #     span 'GIT status'
+          #   end
+          #
+          #   column do
+          #     if updates.to_i == 0
+          #       span 'Up-to-date'
+          #     else
+          #       span "The repository has #{updates} updates."
+          #     end
+          #   end
+          # end
         end
       end
-    end
-
-    panel 'Frontend variables' do
-      columns do
-        column do
-          span 'Debug variables'
-        end
-
-        column do
-          span current_admin_user.debug_variables ? 'Yes' : 'No'
-        end
-      end
-
-      columns do
-        column do
-          span 'Debug template'
-        end
-
-        column do
-          span auto_link current_admin_user.debug_template
-        end
-      end
-    end
-
-    panel 'Application variables' do
-      columns do
-        column do
-          span 'Version'
-        end
-
-        column do
-          span Ecommerce::Application::VERSION.dup
-        end
-      end
-
-      columns do
-        column do
-          span 'Environment'
-        end
-
-        column do
-          span Rails.env
-        end
-      end
-
-      columns do
-        column do
-          span 'Root folder'
-        end
-
-        column do
-          span Rails.root
-        end
-      end
-
-      columns do
-        column do
-          span 'Database'
-        end
-
-        column do
-          span config_db['database']
-        end
-      end
-
-      columns do
-        column do
-          span 'Database adapter'
-        end
-
-        column do
-          span config_db['adapter']
-        end
-      end
-
-      # columns do
-      #   column do
-      #     span 'GIT status'
-      #   end
-      #
-      #   column do
-      #     if updates.to_i == 0
-      #       span 'Up-to-date'
-      #     else
-      #       span "The repository has #{updates} updates."
-      #     end
-      #   end
-      # end
     end
   end # content
 end
