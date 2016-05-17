6a5752d44700368d2bf7c1e1b892b8ade29698d7
Xavi Garcia <xavi.garci@gmail.com>
2016-05-17 20:41:32 +0200

    Refactor Vagrantfile and provision scripts

---
+++ vagrant/setup-env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "********* setup-env.sh ************"
+
+# Stop and remove any existing containers
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+
+# Run postgres container
+docker run -d -p 5432:5432 -v /vagrant:/vagrant -e "POSTGRES_PASSWORD=postgres" --name postgres postgres:9.4.1
+
+# install required packages
+sudo apt-get update
+sudo apt-get install -qy git nodejs imagemagick libpq-dev postgresql-client
+    #libpq-dev   # dependency for pg gem
+
+
+
--- vagrant/start.sh
+++ vagrant/start.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # install new gems and make new migrations
-cd /vagrant
-bundle install
-bundle exec rake db:migrate
+cd /vagrant; bundle install; bundle exec rake db:migrate
 
 # Commands required to ensure correct docker containers are started when the vm is rebooted.
 docker start postgres
 
-#mailcatcher --ip 0.0.0.0
+mailcatcher --ip 0.0.0.0
