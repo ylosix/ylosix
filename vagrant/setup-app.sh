0517a500885ccb84ba6bdd79b92e2dd6a3c3e3b9
Xavi Garcia <xavi.garci@gmail.com>
2016-05-19 21:04:07 +0200

    Refactor Vagrantfile and setup-app.sh script

--- vagrant/setup-app.sh
+++ vagrant/setup-app.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#set environment variables
-source ~/.profile && [ -z "$DATABASE_URL" ] && echo "export DATABASE_URL=postgres://postgres:postgres@127.0.0.1:5432/ecommerce" >> ~/.profile
-source ~/.profile && [ -z "$RAILS_ENV" ] && echo "export RAILS_ENV=development" >> ~/.profile
-
 # generate database
 cd /vagrant; bundle install
 cd /vagrant; bundle exec rake db:create db:schema:load db:gen_demo
